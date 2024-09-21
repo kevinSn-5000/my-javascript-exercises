687badbec76162d60db00d3d9cc921f057bc448b
kevinSn-5000 <179320367+kevinSn-5000@users.noreply.github.com>
2024-09-21 12:05:33 -0600

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringExtra = "";
+    for(let i = 0; i < num; i++){
+        stringExtra = stringExtra + string
+    }
+    if(num < 0){
+        return "ERROR"
+    }
+    else{
+        return stringExtra;
+    }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
