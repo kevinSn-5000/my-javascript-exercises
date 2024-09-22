49563ca63dfbd0ceb781797017f8922d6a423574
kevinSn-5000 <179320367+kevinSn-5000@users.noreply.github.com>
2024-09-22 15:10:18 -0600

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    let celsius = (fahrenheit - 32)/1.8;
+    let result = Math.round(celsius * 10)/10;
+    return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    let fahrenheit = (celsius * 1.8) + 32;
+    let result = Math.round(fahrenheit * 10)/10;
+    return result;
 };
 
 // Do not edit below this line
