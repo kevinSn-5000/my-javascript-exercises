0edced90d86ebf729cd2d89dc4ec2aa7d3394fe5
kevinSn-5000 <179320367+kevinSn-5000@users.noreply.github.com>
2024-09-22 13:14:52 -0600

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...deleted) {
+  //deletes duplicate elements
+    let newArray = [...new Set(array)];
+
+    for(let deleteNumber of deleted){
+      for(let i = 0; i < newArray.length; i++){
+          if(deleteNumber === newArray[i]){
+             newArray.splice(i,1)
+             continue;
+           }
+          else{
+              continue;
+           }
+      }
+    }
+
+    return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
