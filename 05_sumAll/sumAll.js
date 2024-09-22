c3ed87eb2bf6d8363fa0b04e2b1dd1df759de60b
kevinSn-5000 <179320367+kevinSn-5000@users.noreply.github.com>
2024-09-22 14:01:44 -0600

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let totalSum = 0;
+    let startAt;
+    let endAt;
+    if((firstNum < 0 || secondNum < 0) || (Math.floor(firstNum) !== firstNum || Math.floor(secondNum) !== secondNum)){
+        return "ERROR"
+    }
+    if(firstNum >= secondNum){
+            startAt = secondNum;
+            endAt = firstNum;
+    }
+    else if(secondNum >= firstNum){
+            startAt = firstNum;
+            endAt = secondNum; 
+    }
 
+    for(startAt; startAt <= endAt; startAt++){
+        totalSum += startAt;
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
